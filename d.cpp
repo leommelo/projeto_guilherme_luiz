330126cda685abf754aacedf5d0309af0c8ff9b4
= <guilhermeazevedo2212@gmail.com>
2024-10-28 14:53:36 -0300

    adicionado classe D

---
+++ d.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class D{
+
+    private:
+        int D1;
+        int D2;
+    
+    public:
+
+        int getD1(){
+            return D1;
+        }
+
+        void setD1(int value){
+            D1 = value;
+        }
+
+        int getD2(){
+            return D2;
+        }
+
+        void setD2(int value){
+            D2 = value;
+        }
+
+        void MD1(){
+            cout << __func__ << endl;
+        }
+
+        void MD2(){
+            cout << __func__ << endl;
+        }
+};
